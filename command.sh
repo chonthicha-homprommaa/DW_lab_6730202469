6e47b8074d01ac328aea4ea32e70eeaf17fdaf63
chonthicha-homprommaa <chonthicha.ho@kkumail.com>
2026-07-10 06:57:45 +0000

    ou

--- command.sh
+++ command.sh
@@ -1,7 +1,13 @@
 # install dbt-duckdb
 pip install dbt-duckdb
-# initslize dbt project
-pip init <dbt-project-name>
 
+# freeze and save dependencies to requirements.txt
+pip freeze > requirements.txt
+# initialize dbt project
+dbt init <dbt-project-name>
+# move to <dbt-project-name>
 cd <dbt-project-name>
+# test or debug dbt project
 dbt debug
+# move back to the main folder
+cd ..
